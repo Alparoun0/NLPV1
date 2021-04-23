1852978c6daf8f8f2e56239babb686eb29128790
alparoun <alparoun@hotmail.com>
2021-04-23 03:30:40 +0200

    fix git file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,56 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
+  
 module.exports = {
+
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     entry: './src/client/index.js',
-    mode: 'production',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  MiniCssExtract.loader,
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ]
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+            filename: "index.html"
+        }),
+        new MiniCssExtract()
+
+         
     ]
 }
+
